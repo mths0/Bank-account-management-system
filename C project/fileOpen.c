f01536120dd7bb6013e0ac5e78af6f481d4788f2
Mohannad <mhnd9919@gmail.com>
2024-11-05 22:26:22 +0300

    good

---
+++ C project/fileOpen.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+
+    FILE* ptrf;
+
+    ptrf = fopen("newfile.txt","r");
+
+    char text[10000];
+    if(ptrf != NULL){
+        
+        while(fgets(text,1000,ptrf)){
+
+            printf("%s",text);
+
+            
+        }
+    }
+    fclose(ptrf);
+    return 0;
+}

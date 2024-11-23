944f2148ae79ddac6fd6a4ba9b4579ff5cd51750
mths0 <mhnd9919@gmail.com>
2024-11-23 20:20:51 +0300

    init 1

--- C project/BAMSystem.c
+++ C project/BAMSystem.c
@@ -17,49 +17,119 @@ typedef struct{
 }Account;
 
 void menu();
+void addAccount(Account account[], int* num_account);
+void displayAll(Account accounts[], int size);
 int loadAccounts(Account accounts[100]);/*load all data to an array */
-int addAccounts(Account account, int num_account);
+void saveAccounts(Account account[], int num_account);
 
 
 int main(){
     Account accounts[100];
-    int num_of_accounts = loadAccounts(accounts);
+    int choice, num_of_accounts = loadAccounts(accounts);
 
-    Account user1 = {"Mohannad", "mhn@f.com",11241,2000};
+    do{ 
+        menu();
+        scanf("%d",&choice);
 
-  
+        if (choice == 1)
+        {
+            addAccount(accounts,&num_of_accounts);
+            saveAccounts(accounts,num_of_accounts);
+        }
+        else if(choice == 2){
+            displayAll(accounts,num_of_accounts);
+        }else if(choice == 3){
+            displayAll(accounts,num_of_accounts);
+        }else if(choice == 4){
+            displayAll(accounts,num_of_accounts);
+        }else if(choice == 5){
+            displayAll(accounts,num_of_accounts);
+        }else if(choice == 6){
+            displayAll(accounts,num_of_accounts);
+        }else if(choice == 7){
+            displayAll(accounts,num_of_accounts);
+            
+        }else if(choice == 8){
+            system("clear");
+            printf("==========================================================\n");
+            printf("===========Thank you for Banking With BEST BANK===========\n");
+            printf("==========================================================\n");
 
-    
-   
+            
+        }else{
+            printf("\nInvalid choice");
+
+        }
+        
+
+    printf("\n\n");
+    }while(choice != 8);
 
 
-           
     return 0;
 }
+void addAccount(Account accounts[], int* num_account){
 
-int addAccounts(Account account, int num_account){
+    Account newAccount;
+
+    printf("\nEnter your Account number: ");
+    scanf("%d",&newAccount.Acc_number);
+    getchar();
+
+    printf("\nEnter your Name: ");
+    scanf("%s",&newAccount.name);
+
+    printf("\nEnter your Email: ");
+    scanf("%s",&newAccount.email);
+
+    printf("\nEnter your Balance: ");
+    scanf("%f",&newAccount.balance);
+
+    newAccount.number_of_operation= 0;
+    accounts[*num_account] = newAccount;
+    (*num_account)++;
+
+}
+void displayAll(Account accounts[], int size){
+    system("clear");
+    int i;
+    
+    if(!size){
+        printf("There is no accounts.\n");
+        return;
+    }
+    for(i = 0; i<size; i++){
+        printf("\nAccount Number: %ld\n",accounts[i].Acc_number);
+        printf("Account Name: %s\n",accounts[i].name);
+        printf("Account Email: %s\n",accounts[i].email);
+        printf("Account Balance: %ld\n",accounts[i].balance);
+    }
+    
+
+}
+
+void saveAccounts(Account accounts[], int num_account){
 
       // Open a file in binary write mode
     FILE* file = fopen("Accounts.bin", "wb");
     if (file == NULL) {
         perror("Error opening file");
-        return 1;
+        return;
     }
-
     // Write the data to the file
     size_t num_written
-        = fwrite(&account, sizeof(Account), 1, file);
+        = fwrite(&accounts, sizeof(Account), 1, file);
     if (num_written != 1) {
         perror("Error writing to file");
         fclose(file);
-        return 1;
+        return;
     }
     // Close the file
     fclose(file);
     printf("Struct data written to Binary file "
            "successfully.\n");
-
 }
+
 int loadAccounts(Account accounts[]){
 
  FILE* file = fopen("Accounts.bin","rb");
@@ -67,16 +137,15 @@ int loadAccounts(Account accounts[]){
         perror("Error opening file");
         return 1;
     }
-    
     int count = fread(&accounts , sizeof(Account), 1, file);
     return count; /*number of accounts loaded.*/
-
     fclose(file);
 
 }
    
 void menu(){
-    printf("\n====Hello to BEST BANK====\n");
+    // system("clear");
+    printf("====Hello to BEST BANK====\n");
     printf("1 -Add Account\n");
     printf("2 -Update Account\n");
     printf("3 -Delete Account\n");
@@ -84,4 +153,5 @@ void menu(){
     printf("5 -Search Account\n");
     printf("6 -Display All Accounts\n");
     printf("7 -Add Operation\n");
+    printf("8 -Exit\n");
 }
--- C project/tempCodeRunnerFile.c
+++ C project/tempCodeRunnerFile.c
@@ -1,1 +1,2 @@
-e
+ccount accounts[100]);/*load all data to an array */
+void saveAccounts(A
